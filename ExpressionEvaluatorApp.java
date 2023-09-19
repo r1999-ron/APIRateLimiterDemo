abf215719a309a628fc8e85f766ce1ea9aee9952
Ronaksengupta99 <55063393+r1999-ron@users.noreply.github.com>
2023-09-19 23:18:33 +0530

    Update ExpressionEvaluatorApp.java

--- ExpressionEvaluatorApp.java
+++ ExpressionEvaluatorApp.java
@@ -1,24 +1,3 @@
-/*
-Problem Statement
-Write a program that accepts multiple mathematical expressions in bulk and evaluates each of them using any public Web API available. The program should display the result of each expression on the console. Let’s assume that the API only supports 50 requests per second per client whereas your application is expected to evaluate at least 500 expressions per second. Also, the user may initiate more concurrent requests than your application can handle. Suggest an approach to handle this along with the reasoning and implementation of the same.
-
-Rules
-No expressions should be evaluated in the code. All evaluations should be using the Web API.
-You can assume different expressions/operators that are compatible with the API you choose. 
-Example: Some API might use ^ operator for power some might use pow()
-Example
-Input (every line is an expression, evaluate when “end” is provided as an expression)
-2 * 4 * 4
-5 / (7 - 5)
-sqrt(5^2 - 4^2)
-sqrt(-3^2 - 4^2)
-end
-Output
-2 * 4 * 4 => 32
-5 / (7 - 5) => 2.5
-sqrt(5^2 - 4^2) => 3
-sqrt(-3^2 - 4^2) = 5i
-*/
 package com.ronak.assignment.task2;
 
 import java.io.BufferedReader;
